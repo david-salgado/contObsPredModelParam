bcb157dbcb1eaa35334ad87870eb2abe632ccb20
david-salgado <david.salgado.fernandez@gmail.com>
2019-11-01 09:03:45 +0100

    Correct ComputeProb to make it similar to ComputeObsErrorSTD

--- R/ComputeErrorProb.R
+++ R/ComputeErrorProb.R
@@ -2,13 +2,13 @@
 #'
 #'
 #' @param object Object of class \linkS4class{contObsPredModelParam} containing the statistical
-#' units whose probability of measurement error for the variables is to be computed.
+#' units whose probability of measurement error for each variable is to be computed.
 #'
 #' @param Param Object of virtual class \linkS4class{ErrorProbParam} with the parameters determining
-#' the variables and the method of computation of the probability of measurement error.
+#' the method of computation of the probability of measurement error of each statistical unit.
 #'
 #' @return Object of class \linkS4class{contObsPredModelParam} with the measurement error
-#' probabilities computed for each variable specified in the parameters and each statistical unit.
+#' probabilities computed for each variable and each statistical unit.
 #'
 #'
 #' @examples
@@ -54,31 +54,40 @@ setGeneric("ComputeErrorProb", function(object, Param) {standardGeneric("Compute
 #'
 #' @export
 setMethod(f = "ComputeErrorProb",
-          signature = c("contObsPredModelParam", "ErrorProbParam"),
+          signature = c("contObsPredModelParam", "ErrorProbMLEParam"),
           function(object, Param){
 
-          RawPeriods <- getPeriods(getRawData(Param))
-          EdPeriods <- getPeriods(getEdData(Param))
-          CommonPeriods <- intersect(EdPeriods, RawPeriods)
-          RawData <- subPeriods(getRawData(Param), CommonPeriods)
-          EdData <- subPeriods(getEdData(Param), CommonPeriods)
-          Units <- getUnits(getData(object))
-          IDQuals <- names(Units)
-          if (length(CommonPeriods) == 0) {
+            RawPeriods <- getPeriods(getRawData(Param))
+            EdPeriods <- getPeriods(getEdData(Param))
+            CommonPeriods <- intersect(EdPeriods, RawPeriods)
+            RawData <- subPeriods(getRawData(Param), CommonPeriods)
+            EdData <- subPeriods(getEdData(Param), CommonPeriods)
+            Units <- getUnits(getData(object))
+            IDQuals <- names(Units)
+            if (length(CommonPeriods) == 0) {
 
-            stop('[contObsPredModelParam: validity] No common time periods between RawData and EdData.')
+              stop('[contObsPredModelParam: validity] No common time periods between RawData and EdData.')
 
-          }
+            }
 
-          Variables <- getVarNames(Param)
-          PeriodList <- lapply(CommonPeriods, function(Period){
+            Variables <- getVarNames(Param)
+            PeriodList <- lapply(CommonPeriods, function(Period){
+
+              # localVariables <- ExtractNames(Variables)
+              # localRawData.dm <- dcast_StQ(RawData[[Period]], localVariables)
+              # localRawData.dm <- merge(Units, localRawData.dm, all.x = TRUE, by = IDQuals)
+              # localEdData.dm <- dcast_StQ(EdData[[Period]], localVariables)
+              # localEdData.dm <- merge(Units, localEdData.dm, all.x = TRUE, by = IDQuals)
+              # localData.dm <- merge(localEdData.dm, localRawData.dm)
 
               localVariables <- ExtractNames(Variables)
-              localRawData.dm <- dcast_StQ(RawData[[Period]], localVariables)
+              localRawData.dm <- dcast_StQ(RawData[[Period]], localVariables)[, c(IDQuals, Variables), with = FALSE]
               localRawData.dm <- merge(Units, localRawData.dm, all.x = TRUE, by = IDQuals)
-              localEdData.dm <- dcast_StQ(EdData[[Period]], localVariables)
+              localEdData.dm <- dcast_StQ(EdData[[Period]], localVariables)[, c(IDQuals, Variables), with = FALSE]
               localEdData.dm <- merge(Units, localEdData.dm, all.x = TRUE, by = IDQuals)
               localData.dm <- merge(localEdData.dm, localRawData.dm)
+
+
               for (Var in Variables){
 
                 localData.dm[, Period := Period]
@@ -88,58 +97,58 @@ setMethod(f = "ComputeErrorProb",
 
               }
               return(localData.dm)
-          })
+            })
 
-          nPeriods <- length(PeriodList)
-          ProbList.dt <- rbindlist(PeriodList)
+            nPeriods <- length(PeriodList)
+            ProbList.dt <- rbindlist(PeriodList)
 
-          output <- lapply(Variables, function(Var){
+            output <- lapply(Variables, function(Var){
 
-            localOutput <- ProbList.dt[, sum(get(paste0('Unit.p.', Var)), na.rm = TRUE) / nPeriods, by = IDQuals]
-            setnames(localOutput, 'V1', Var)
-            return(localOutput)
+              localOutput <- ProbList.dt[, sum(get(paste0('Unit.p.', Var)), na.rm = TRUE) / nPeriods, by = IDQuals]
+              setnames(localOutput, 'V1', Var)
+              return(localOutput)
 
-          })
-          output <- Reduce(function(x, y){merge(x, y, by = intersect(names(x), names(y)))}, output, init = output[[1]])
+            })
+            output <- Reduce(function(x, y){merge(x, y, by = intersect(names(x), names(y)))}, output, init = output[[1]])
 
-          DomainNames <- getDomainNames(Param)
-          Domains <- dcast_StQ(getData(object), ExtractNames(DomainNames))
-          output <- merge(output, Domains, by = IDQuals, all.x = TRUE)
-          output <- Impute(output, getImputation(Param))
+            DomainNames <- getDomainNames(Param)
+            Domains <- dcast_StQ(getData(object), ExtractNames(DomainNames))
+            output <- merge(output, Domains, by = IDQuals, all.x = TRUE)
+            output <- Impute(output, getImputation(Param))
 
-          DD <- getDD(getData(object))
-          VNC <- getVNC(DD)
-          for (Var in Variables){
+            DD <- getDD(getData(object))
+            VNC <- getVNC(DD)
+            for (Var in Variables){
 
-            localVar <- ExtractNames(Var)
-            newDDdt <- DD$MicroData[Variable == localVar]
-            newDDdt[, Variable := paste0('ErrorProb', localVar)]
+              localVar <- ExtractNames(Var)
+              newDDdt <- DD$MicroData[Variable == localVar]
+              newDDdt[, Variable := paste0('ErrorProb', localVar)]
 
-            auxUnitName <- IDDDToUnitNames(Var, DD)
-            newVNCdt <- VNC$MicroData[UnitName == auxUnitName | IDQual != '' | NonIDQual != '']
-            newVNCdt[UnitName == auxUnitName, IDDD := paste0('ErrorProb', IDDD)]
-            newVNCdt[UnitName == auxUnitName, UnitName := paste0('ErrorProb', UnitName)]
-            newVNC <- BuildVNC(list(MicroData = newVNCdt))
+              auxUnitName <- IDDDToUnitNames(Var, DD)
+              newVNCdt <- VNC$MicroData[UnitName == auxUnitName | IDQual != '' | NonIDQual != '']
+              newVNCdt[UnitName == auxUnitName, IDDD := paste0('ErrorProb', IDDD)]
+              newVNCdt[UnitName == auxUnitName, UnitName := paste0('ErrorProb', UnitName)]
+              newVNC <- BuildVNC(list(MicroData = newVNCdt))
 
-            newDD <- DD(VNC = newVNC, MicroData = newDDdt)
-            newDD <- DD + newDD
+              newDD <- DD(VNC = newVNC, MicroData = newDDdt)
+              newDD <- DD + newDD
 
-            newData <- output[, c(IDQuals, Var), with = FALSE]
+              newData <- output[, c(IDQuals, Var), with = FALSE]
 
-            setnames(newData, Var, paste0('ErrorProb', IDDDToUnitNames(Var, DD)))
-            newStQ <- melt_StQ(newData, newDD)
-            setData(object) <- getData(object) + newStQ
-          }
+              setnames(newData, Var, paste0('ErrorProb', IDDDToUnitNames(Var, DD)))
+              newStQ <- melt_StQ(newData, newDD)
+              setData(object) <- getData(object) + newStQ
+            }
 
-          setErrorProb(object) <- c(getErrorProb(object), paste0('ErrorProb', Variables))
+            setErrorProb(object) <- c(getErrorProb(object), paste0('ErrorProb', Variables))
 
-          if (length(getObjVariables(object)) == 0) {
+            if (length(getObjVariables(object)) == 0) {
 
-            setObjVariables(object)<- getVarNames(Param)
+              setObjVariables(object)<- getVarNames(Param)
 
-          }
+            }
 
-          return(object)
+            return(object)
 
           }
 )
